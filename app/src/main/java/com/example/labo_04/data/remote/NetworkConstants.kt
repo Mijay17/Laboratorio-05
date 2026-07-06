7da23b5c8cad8f2e971fcd900d5ddb6a1363d17f
Junior <david2505j@gmail.com>
2026-07-06 18:43:09 -0500

    Laboratorio 8

--- app/src/main/java/com/example/labo_04/data/remote/NetworkConstants.kt
+++ app/src/main/java/com/example/labo_04/data/remote/NetworkConstants.kt
@@ -5,4 +5,10 @@ object NetworkConstants {
     // const val BASE_URL  = "http://illarek.org/"
 
     const val PROJECT_SLUG = "c19200185"
+
+    /**
+     * Client ID de Google para la autenticación.
+     * Reemplaza esto con tu Web Client ID de la consola de Google Cloud.
+     */
+    const val GOOGLE_WEB_CLIENT_ID = "142129882974-08o2chv227j7iqnqm9dcc55ctf0ms2kq.apps.googleusercontent.com"
 }
